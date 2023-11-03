5563348cfd1a4041f3950f89ac6e4a2ffc7f032e
Muhammad Ahsan <mdotihsan@gmail.com>
2023-11-03 12:44:06 +0500

    new files

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("APP_HOST", "0.0.0.0"),
+  port: env.int("NODE_PORT", 1338),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS"),
   },
 });
